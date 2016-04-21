7d32c37e247621c7b7cafa54d6684351cac48606
ekimlinger <ekimlinger@gmail.com>
2016-04-21 14:34:06 -0500

    Finished cleanup

--- server/app.js
+++ server/app.js
@@ -7,36 +7,12 @@ var bodyParser = require('body-parser');
 var router = require('./modules/index.js');
 var postRouter = require('./modules/postRouter.js');
 
-<<<<<<< HEAD
-//MONGO
-var mongoose = require("mongoose");
-var mongoURI =
-  process.env.MONGOLAB_URI ||
-  process.env.MONGOHQ_URL ||
-  'mongodb://localhost/mango_grove';
-
-var MongoDB = mongoose.connect(mongoURI).connection;
-
-MongoDB.on("error", function(err){
-    console.log("Mongo Connection Error: ", err);
-});
-
-MongoDB.once("open", function(err){
-    console.log("Mongo Connection Open");
-});
-=======
 
 var db = require('./modules/db');
 var router = require('./modules/index.js');
 
 //models
 var Post = require('./models/posts');
-
-
-//ROUTES
-var router = require('./modules/index.js');
-var postRouter = require('./modules/postRouter.js');
->>>>>>> a4dfd80d7237a005b24ca4f4e0eba769de65ee4e
 
 
 //BODYPARSER
