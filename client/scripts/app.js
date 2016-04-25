d4910753a9253b30004a531e8c4aecdaf828079f
bpeterson1663 <bpeterson1663@gmail.com>
2016-04-25 11:14:49 -0500

    Global and Individual Feeds working

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,25 +1,12 @@
 var newMessage = {};//New Message object to be sent down to the database
-
-//for db testing
-// function sendData(taskData){
-//   $.ajax({
-//     type: 'POST',
-//     url: '/post',
-//     data: taskData,
-//     success : function (data){
-//       console.log(data);
-//     }
-//   });
-//
-// }
-
+var allMessages;
 $(document).ready(function(){
     //when submit button is pressed in the postMessageForm
     $('#postMessageForm').on('submit', createPost);
     //Filters global
-    $('#shoutOut').on('click',getGlobalMessages);
-    $('#mangoMoment').on('click',getGlobalMessages);
-    $('#affirmation').on('click',getGlobalMessages);
+    $('#shoutOut').on('click',showAllShoutOuts);
+    $('#mangoMoment').on('click',showAllMangoMoments);
+    $('#affirmation').on('click',showAllAffirmations);
     $('#all').on('click',getGlobalMessages);
 
     getGlobalMessages();
@@ -52,24 +39,100 @@ function getGlobalMessages(){
   var type = $(this).data('type') || 'all';
   //When function is called pass in type of "all", "so", "mm","af"
   //start of get all messages ajax call
-  var amount = 1;
+  var amount = 20;//Limits how many messages are displayed on the dom at any given time
   var time = new Date(Date.now());
   $.ajax({
     type: 'GET',
     //MAKE SURE TO CHANGE THE URL ROUTE --change
     url: '/message/global/'+type+'/'+amount+'/'+time,
-    success: function(response){
-      console.log("Successful Get Request: ", response);
-    }
+    success: showAllGlobalFeed
       //MOST LIKELY WILL NEED AN APPEND TO DOM FUNCTION HERE TO DISPLAY NEW FEED
       //WILL HAVE TO EMPTY THE DIV FIRST AND THEN REPOST ALL NEW INFO --change
   });
 }
 
-function loadGlobalFeed(response){//response is the data coming back from the database. NOT SURE EXACTLY HOW IT IS GOING TO COME BACK
+function showAllGlobalFeed(response){//response is the data coming back from the database. NOT SURE EXACTLY HOW IT IS GOING TO COME BACK
+  allMessages = response;
   //empty out the div container on the DOM that stores the messages to refresh
-  //append comments to the DOM
+  console.log("Successful Get Request: ", response);
+  $('.comment-container').empty();
   console.log("Response from get route is: ", response);
   //loop through the array and append INFO
+  //append info to comment-container
+  for(var i = 0; i <response.length; i++){
+    var comment = response[i];//store response into comment for readability
+    console.log("Name: ", comment.name);
+    console.log("Content: ", comment.content);
+    $('.comment-container').append('<div class="media animated fadeInRight"></div>');//creates each individual comment
+    var $el = $('.comment-container').children().last();
 
+    $el.append('<a class="forum-avatar" href="#"><img src="/vendors/Static_Seed_Project/img/a3.jpg" class="img-circle" alt="image"><div class="author-info"><strong>Posts:</strong> 543<br/><strong>Date of Post:</strong>'+comment.date_created+'<br/></div></a>');
+    $el.append('<div class="media-body"><h4 class="media-heading">Hampden-Sydney College in Virginia</h4>'+comment.content+'<br/><br/>- '+comment.name+'</div>');
+  }
 }
+
+  function showAllMangoMoments(){//response is the data coming back from the database. NOT SURE EXACTLY HOW IT IS GOING TO COME BACK
+    //empty out the div container on the DOM that stores the messages to refresh
+    $('.comment-container').empty();
+    //loop through the array and append INFO
+    //append info to comment-container
+    for(var i = 0; i <allMessages.length; i++){
+      var comment = allMessages[i];//store response into comment for readability
+      if(comment.type == 'mm'){
+      $('.comment-container').append('<div class="media animated fadeInRight"></div>');//creates each individual comment
+      var $el = $('.comment-container').children().last();
+
+      $el.append('<a class="forum-avatar" href="#"><img src="/vendors/Static_Seed_Project/img/a3.jpg" class="img-circle" alt="image"><div class="author-info"><strong>Posts:</strong> 543<br/><strong>Date of Post:</strong>'+comment.date_created+'<br/></div></a>');
+      $el.append('<div class="media-body"><h4 class="media-heading">Hampden-Sydney College in Virginia</h4>'+comment.content+'<br/><br/>- '+comment.name+'</div>');
+    }
+  }
+}
+function showAllShoutOuts(){//response is the data coming back from the database. NOT SURE EXACTLY HOW IT IS GOING TO COME BACK
+  //empty out the div container on the DOM that stores the messages to refresh
+  $('.comment-container').empty();
+  //loop through the array and append INFO
+  //append info to comment-container
+  for(var i = 0; i <allMessages.length; i++){
+    var comment = allMessages[i];//store response into comment for readability
+    if(comment.type == 'so'){
+    $('.comment-container').append('<div class="media animated fadeInRight"></div>');//creates each individual comment
+    var $el = $('.comment-container').children().last();
+
+    $el.append('<a class="forum-avatar" href="#"><img src="/vendors/Static_Seed_Project/img/a3.jpg" class="img-circle" alt="image"><div class="author-info"><strong>Posts:</strong> 543<br/><strong>Date of Post:</strong>'+comment.date_created+'<br/></div></a>');
+    $el.append('<div class="media-body"><h4 class="media-heading">Hampden-Sydney College in Virginia</h4>'+comment.content+'<br/><br/>- '+comment.name+'</div>');
+  }
+}
+}
+function showAllAffirmations(){//response is the data coming back from the database. NOT SURE EXACTLY HOW IT IS GOING TO COME BACK
+  //empty out the div container on the DOM that stores the messages to refresh
+  $('.comment-container').empty();
+  //loop through the array and append INFO
+  //append info to comment-container
+  for(var i = 0; i <allMessages.length; i++){
+    var comment = allMessages[i];//store response into comment for readability
+    if(comment.type == 'af'){
+    $('.comment-container').append('<div class="media animated fadeInRight"></div>');//creates each individual comment
+    var $el = $('.comment-container').children().last();
+
+    $el.append('<a class="forum-avatar" href="#"><img src="/vendors/Static_Seed_Project/img/a3.jpg" class="img-circle" alt="image"><div class="author-info"><strong>Posts:</strong> 543<br/><strong>Date of Post:</strong>'+comment.date_created+'<br/></div></a>');
+    $el.append('<div class="media-body"><h4 class="media-heading">Hampden-Sydney College in Virginia</h4>'+comment.content+'<br/><br/>- '+comment.name+'</div>');
+  }
+}
+}
+  //example of comment-container
+  // <div class="media">
+  //     <a class="forum-avatar" href="#">
+  //         <img src="/vendors/Static_Seed_Project/img/a3.jpg" class="img-circle" alt="image">
+  //         <div class="author-info">
+  //             <strong>Posts:</strong> 543<br/>
+  //             <strong>Joined:</strong> June 21.2015<br/>
+  //         </div>
+  //     </a>
+  //     <div class="media-body">
+  //         <h4 class="media-heading">Hampden-Sydney College in Virginia</h4>
+  //          All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures
+  //         <br/><br/>
+  //         - Monica Jackson
+  //         UX developer
+  //     </div>
+  // </div>
