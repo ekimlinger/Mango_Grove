25081918bc03c73ce8a94a8262aede9930a2b800
Xiaoran  Wang <xiaoranwang@Xiaorans-MacBook-Air.local>
2016-04-25 13:13:52 -0500

    update posts comment

--- server/models/messages.js
+++ server/models/messages.js
@@ -17,7 +17,7 @@ var Message = new Schema({
     type: {type: String, required: true},
     content: {type: String, required: true},
     date_created: {type: Date, default : Date.now},
-    comments : {type: Array, default: []},
+    comments : {type: Array, default: [], required: false},
     name : {type: String, required: true},
     email : {type: String, required: true},
     location: {type: String, required: false},
@@ -28,4 +28,3 @@ var Message = new Schema({
 });
 
 module.exports = mongoose.model("Message", Message);
-module.exports = mongoose.model("Comment", Comment);
