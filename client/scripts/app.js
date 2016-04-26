f67801ecf54d6d1529200df5b5c9385787422b5e
bpeterson1663 <bpeterson1663@gmail.com>
2016-04-25 23:33:06 -0500

    Loading messages to DOM functionality improved

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,39 +1,53 @@
 var newMessage = {};//New Message object to be sent down to the database
-var allMessages;
+
 $(document).ready(function(){
-
+    var messageType = "all";
     dismissCheckbox();//checks if do not show again button is checked
-
+    showMessages(messageType);//show all messages on page load
     //When Compose Buttons are clicked for guests
-    $('#composeSO').on('click', function(){
-      $('#so').replaceWith('<input type="radio" checked name="type" value="so" id="so">');
-      $('#guestCommentModal').modal('show');
+    $('.compose').on('click', composeMessage);
+    //Event Handler that will Filter global messages
+    $('.filter-messages').on('click',function(){
+      messageType = $(this).data('type');
+      showMessages(messageType);
     });
-    $('#composeAF').on('click', function(){
-      $('#af').replaceWith('<input type="radio" checked name="type" value="af" id="af">');
-      $('#guestCommentModal').modal('show');
-    });
-    $('#composeMM').on('click', function(){
-      $('#mm').replaceWith('<input type="radio" checked name="type" value="mm" id="mm">');
-      $('#guestCommentModal').modal('show');
-    });
-
-    //Event Handlers that will Filter global messages
-    $('#shoutOut').on('click',showAllShoutOuts);
-    $('#mangoMoment').on('click',showAllMangoMoments);
-    $('#affirmation').on('click',showAllAffirmations);
-    $('#all').on('click',showAllMessages);
-
     //when submit button is pressed in the guest_comment_modals
     $('#createGuestPost').on('click', createPost);
     //WILL NEED #createUserPost event handler
-    showAllMessages();
-
 });
+
+function composeMessage(){
+  var composeType = $(this).data('type');
+  if(composeType == "mm"){
+    $('#mm').replaceWith('<input type="radio" checked name="type" value="mm" id="mm">');
+    $('#guestCommentModal').modal('show');
+  }
+  else if(composeType == "af"){
+    $('#af').replaceWith('<input type="radio" checked name="type" value="af" id="af">');
+    $('#guestCommentModal').modal('show');
+  }
+  else if(composeType == "so"){
+    $('#so').replaceWith('<input type="radio" checked name="type" value="so" id="so">');
+    $('#guestCommentModal').modal('show');
+  }
+
+}
+//Shows specific Messages -- Mango Momment, Affirmations, Shout Outs or All of them
+function showMessages(messageType){
+  var type = messageType;
+  //start of get all messages ajax call
+  var amount = 20;//Limits how many messages are displayed on the dom at any given time
+  var time = new Date(Date.now());
+  $.ajax({
+    type: 'GET',
+    //MAKE SURE TO CHANGE THE URL ROUTE --change
+    url: '/message/global/'+type+'/'+amount+'/'+time,
+    success: loadGlobalFeed //loads messages on the success of the ajax call
+  });
+}
 
 //Create Post Function
 function createPost(event){
-
     event.preventDefault();
     //grab the information from the compose message modal NEED THE ID FROM THE FORM --CHANGE
     var messageArray = $('#postMessageForm').serializeArray();
@@ -42,33 +56,32 @@ function createPost(event){
       newMessage[element.name] = element.value;
     });
     newMessage.global = true;
-    console.log("Data coming off form: ", newMessage);
-    //start of post new message ajax call
     //reset input field values
     $('#guestTextarea').val('');
     $('#guestEmail').val('');
     $('#username').val('');
-
     $.ajax({
       type: 'POST',
       url: '/message',
       data: newMessage, //Pass newMessage to the Database
-      success: showAllMessages,
-      async: false
+      success: addNewMessageToFeed //call addNewMessageToFeed function to display new post right away
     });
+}
+//Append New Message to the Top of the Feed
+function addNewMessageToFeed(response){
+  $('.comment-container').prepend('<div class="media animated fadeInRight"></div>');
+  var $el = $('.comment-container').children().first();
+
+  $el.append('<a class="forum-avatar" href="#"><img src="/vendors/Static_Seed_Project/img/a3.jpg" class="img-circle" alt="image"><div class="author-info"><strong>Posts:</strong> 543<br/><strong>Date of Post:</strong>'+response.date_created+'<br/></div></a>');
+  $el.append('<div class="media-body"><h4 class="media-heading">Hampden-Sydney College in Virginia</h4>'+response.content+'<br/><br/>- '+response.name+'</div>');
 
 }
 
-
-//Shows all messages
+//Loads Messages to GlobalFeed
 function loadGlobalFeed(response){
-
-  allMessages = response;//stores the response from the database into a global variable allMessages to be used in other functions
-
-  //empty out the div container on the DOM that stores the messages to refresh
+  //empty out the div container on the DOM that stores the messages to refresh the page
   $('.comment-container').empty();
-  //loop through the array and append INFO
-  //append info to comment-container
+  //append info to comment-container by looping through the array
   for(var i = 0; i <response.length; i++){
     var comment = response[i];//store response into comment for readability
     $('.comment-container').append('<div class="media animated fadeInRight"></div>');//creates each individual comment
@@ -76,71 +89,6 @@ function loadGlobalFeed(response){
 
     $el.append('<a class="forum-avatar" href="#"><img src="/vendors/Static_Seed_Project/img/a3.jpg" class="img-circle" alt="image"><div class="author-info"><strong>Posts:</strong> 543<br/><strong>Date of Post:</strong>'+comment.date_created+'<br/></div></a>');
     $el.append('<div class="media-body"><h4 class="media-heading">Hampden-Sydney College in Virginia</h4>'+comment.content+'<br/><br/>- '+comment.name+'</div>');
-  }
-}
-//Function Grabs all Global Messages
-function showAllMessages(){
-  var type = $(this).data('type') || 'all';
-  //When function is called pass in type of "all", "so", "mm","af"
-  //start of get all messages ajax call
-  var amount = 20;//Limits how many messages are displayed on the dom at any given time
-  var time = new Date(Date.now());
-  $.ajax({
-    type: 'GET',
-    //MAKE SURE TO CHANGE THE URL ROUTE --change
-    url: '/message/global/'+type+'/'+amount+'/'+time,
-    success: loadGlobalFeed //show all messages on the success of the ajax call
-  });
-}
-//function that justs shows Mango Moments when called
-function showAllMangoMoments(){
-    //empty out the div container on the DOM that stores the messages to refresh
-    $('.comment-container').empty();
-    //loop through the array and append INFO
-    //append info to comment-container
-    for(var i = 0; i <allMessages.length; i++){
-      var comment = allMessages[i];//store response into comment for readability
-      if(comment.type == 'mm'){
-      $('.comment-container').append('<div class="media animated fadeInRight"></div>');//creates each individual comment
-      var $el = $('.comment-container').children().last();
-
-      $el.append('<a class="forum-avatar" href="#"><img src="/vendors/Static_Seed_Project/img/a3.jpg" class="img-circle" alt="image"><div class="author-info"><strong>Posts:</strong> 543<br/><strong>Date of Post:</strong>'+comment.date_created+'<br/></div></a>');
-      $el.append('<div class="media-body"><h4 class="media-heading">Hampden-Sydney College in Virginia</h4>'+comment.content+'<br/><br/>- '+comment.name+'</div>');
-    }
-  }
-}
-//Function that shows just Shout Outs when Called
-function showAllShoutOuts(){
-  //empty out the div container on the DOM that stores the messages to refresh
-  $('.comment-container').empty();
-  //loop through the array and append info
-  //append info to comment-container
-  for(var i = 0; i <allMessages.length; i++){
-    var comment = allMessages[i];//store response into comment for readability
-    if(comment.type == 'so'){
-    $('.comment-container').append('<div class="media animated fadeInRight"></div>');//creates each individual comment
-    var $el = $('.comment-container').children().last();
-
-    $el.append('<a class="forum-avatar" href="#"><img src="/vendors/Static_Seed_Project/img/a3.jpg" class="img-circle" alt="image"><div class="author-info"><strong>Posts:</strong> 543<br/><strong>Date of Post:</strong>'+comment.date_created+'<br/></div></a>');
-    $el.append('<div class="media-body"><h4 class="media-heading">Hampden-Sydney College in Virginia</h4>'+comment.content+'<br/><br/>- '+comment.name+'</div>');
-  }
-}
-}
-//Function that justs shows Affirmation Posts when called
-function showAllAffirmations(){
-  //empty out the div container on the DOM that stores the messages to refresh
-  $('.comment-container').empty();
-  //loop through the array and append INFO
-  //append info to comment-container
-  for(var i = 0; i <allMessages.length; i++){
-    var comment = allMessages[i];//store response into comment for readability
-    if(comment.type == 'af'){
-    $('.comment-container').append('<div class="media animated fadeInRight"></div>');//creates each individual comment
-
-    var $el = $('.comment-container').children().last();
-    $el.append('<a class="forum-avatar" href="#"><img src="/vendors/Static_Seed_Project/img/a3.jpg" class="img-circle" alt="image"><div class="author-info"><strong>Posts:</strong> 543<br/><strong>Date of Post:</strong>'+comment.date_created+'<br/></div></a>');
-    $el.append('<div class="media-body"><h4 class="media-heading">Hampden-Sydney College in Virginia</h4>'+comment.content+'<br/><br/>- '+comment.name+'</div>');
-    }
   }
 }
 //dismiss Checkbox fucntion for Welcome modal
