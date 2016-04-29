d7c00f902dc6bf6902853a123886a1ba7d1665c5
ekimlinger <ekimlinger@gmail.com>
2016-04-29 15:34:12 -0500

    Ladies and Gentlemen.. I give you.... DYNAMIC ICONS

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -57,10 +57,24 @@ function loadGlobalFeed(response){//Loads Messages to GlobalFeed
   for(var i = 0; i <response.length; i++){  //append info to comment-container by looping through the array
 
     var message = response[i];//store response into comment for readability
+
+    var iconType;             // Sets icon type to be displayed on dom
+    switch (message.type) {
+      case "so":
+        iconType = "noun_24896_cc_mod"
+        break;
+      case "mm":
+        iconType = "mango"
+        break;
+      case "af":
+        iconType = "noun_75102_cc"
+        break;
+    }
+
     $('.social-feed-box').append('<div class="media animated fadeInRight underline"></div>');//creates each individual comment
     var $el = $('.social-feed-box').children().last();
 
-    $el.append('<div class="post-icon"><img src="/assets/views/images/noun_24896_cc_mod.png" height="30" width="30" /></div>');
+    $el.append('<div class="post-icon"><img src="/assets/views/images/'+ iconType +'.png" height="30" width="30" /></div>');
     $el.append('<div class="social-avatar"><a href="" class="pull-left"><img alt="image" src="/vendors/Static_Seed_Project/img/a1.jpg"></a><div class="media-body"><a href="#">'+message.name+'</a><small class="text-muted">'+message.date_created+'</small></div></div>');
     $el.append('<div class="social-body"><p>'+message.content+'</p><div class="btn-group"><button class="btn btn-white btn-xs"><i class="fa fa-thumbs-up"></i> Like this!</button><button class="btn btn-white btn-xs" id="messageComment" data-toggle="modal" data-target="#guestMessageCommentModal" data-id="'+message._id+'"><i class="fa fa-comments"></i> Comment</button></div><button class="btn btn-white btn-xs flag-button small-type"><i class="fa fa-flag"></i> Report inappropriate post</button></div>');
     $el.append('<div class="social-footer" id="'+message._id+'"></div>');
