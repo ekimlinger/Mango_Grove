d3858556c105fe4a27ba674d17b510be97d73b23
bpeterson1663 <bpeterson1663@gmail.com>
2016-05-03 11:49:12 -0500

    working on appending new post

--- server/public/assets/scripts/user_modal_app.js
+++ server/public/assets/scripts/user_modal_app.js
@@ -1,7 +1,8 @@
 var newMessage = {};
-var communityList = ["Carlson School","Macalester School"];
+var communityList = ["Carlson School of Management","Macalester"];
 var community = communityList[0];
 var messageType = "all";
+var globalView = "";
 var dateOptions = {     // Date formatting options
     year: "numeric", month: "short",
     day: "numeric", hour: "2-digit", minute: "2-digit"
@@ -9,7 +10,6 @@ var dateOptions = {     // Date formatting options
 
 $(document).ready(function(){
     $('#createCommunityPost').on('click',createCommunityPost);
-
     for(var i = 0; i < communityList.length; i++){
       $('.community-list').append('<label><input type="checkbox" name="location" value="'+communityList[i]+'"> '+communityList[i]+'</label> ');
     }
@@ -24,6 +24,7 @@ $(document).ready(function(){
 
 function createCommunityPost(){
   messageType = $('.modal-content').data('messageType');
+
   console.log("Message Type from the data method: ", messageType);
   var checked = $("input[type=checkbox]:checked").length;
   if(!checked) {
@@ -38,6 +39,7 @@ function createCommunityPost(){
   $.each(messageArray, function(index, element){//grab information off the form and stores it into the newMessage variable
     if(element.name == "location"){
       newMessage.location.push(element.value);//push multiple locations to location key
+      newMessage[element.name] = element.value;
     }
     else{
       newMessage[element.name] = element.value;
@@ -56,18 +58,24 @@ function createCommunityPost(){
 
 function addNewMessageToFeed(response){//Append New Message to the Top of the Feed
   var message = response;
-
+  console.log("HERE");
   // Formats date/time
   var newDate = new Date(message.date_created);
   message.date_created = newDate.toLocaleTimeString("en-us", dateOptions);
 
-  if(message.global == false && message.location && (messageType == newMessage.type)){
+  if((newMessage.global == true) && (globalView == "global")){
+    $('.social-feed-box').prepend('<div class="media animated fadeInRight"></div>');//creates each individual comment
+    var $el = $('.social-feed-box').children().first();
+
+    $el.append('<div class="social-avatar"><a href="" class="pull-left"><img alt="image" src="/vendors/Static_Seed_Project/img/a1.jpg"></a><div class="media-body"><a href="#">'+message.name+'</a><small class="text-muted">'+message.date_created+'</small></div></div>');
+    $el.append('<div class="social-body"><p>'+message.content+'</p><div class="btn-group"><button class="btn btn-white btn-xs"><i class="fa fa-thumbs-up"></i> Like this!</button><button class="btn btn-white btn-xs"><i class="fa fa-comments"></i> Comment</button><button class="btn btn-white btn-xs"><i class="fa fa-share"></i> Share</button></div></div><div id="'+message._id+'"></div>');
+  }
+
+  else if(message.location && (messageType == newMessage.type)){
         $('.social-feed-box').prepend('<div class="media animated fadeInRight"></div>');//creates each individual comment
         var $el = $('.social-feed-box').children().first();
 
         $el.append('<div class="social-avatar"><a href="" class="pull-left"><img alt="image" src="/vendors/Static_Seed_Project/img/a1.jpg"></a><div class="media-body"><a href="#">'+message.name+'</a><small class="text-muted">'+message.date_created+'</small></div></div>');
         $el.append('<div class="social-body"><p>'+message.content+'</p><div class="btn-group"><button class="btn btn-white btn-xs"><i class="fa fa-thumbs-up"></i> Like this!</button><button class="btn btn-white btn-xs"><i class="fa fa-comments"></i> Comment</button><button class="btn btn-white btn-xs"><i class="fa fa-share"></i> Share</button></div></div><div id="'+message._id+'"></div>');
-  }else{
-
   }
 }
