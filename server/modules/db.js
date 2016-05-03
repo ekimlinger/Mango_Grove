a213f186483dc7eeab4a423be7ace596216f06e9
ekimlinger <ekimlinger@gmail.com>
2016-05-03 12:46:41 -0500

    Changed mongoURI

--- server/modules/db.js
+++ server/modules/db.js
@@ -1,8 +1,8 @@
 //MONGO
 var mongoose = require("mongoose");
 var mongoURI =
-  process.env.MONGOLAB_URI ||
-  process.env.MONGOHQ_URL ||
+  // process.env.MONGOLAB_URI ||
+  // process.env.MONGOHQ_URL ||
   'mongodb://admin:MangoGrove@ds013212.mlab.com:13212/heroku_btq31zn7';
 
 var MongoDB = mongoose.connect(mongoURI).connection;
